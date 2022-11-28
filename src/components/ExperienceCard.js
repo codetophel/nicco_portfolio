40f62cf3672ea5a72b8598c37d392ce9dd1cd0e6
codetophel <nicodemusakande@gmail.com>
2022-11-28 20:38:41 +0100

    modified:   src/components/ExperienceCard.js

--- src/components/ExperienceCard.js
+++ src/components/ExperienceCard.js
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 
 const ExperienceCard = () => {
   return (
-    <article className='mx-auto flex flex-shrink-0 flex-col items-center space-y-7 rounded-lg bg-[whitesmoke] p-10 text-center opacity-50 hover:opacity-100 dark:bg-[#292929]  md:w-[600px] xl:w-[900px]'>
+    <article className='mx-auto flex flex-shrink-0 flex-col items-center space-y-7 rounded-lg bg-[whitesmoke] p-10 text-center opacity-80 hover:opacity-100 dark:bg-[#292929]  md:w-[600px] xl:w-[900px]'>
       <motion.img
         initial={{
           y: -100,
@@ -22,55 +22,55 @@ const ExperienceCard = () => {
         <p className='text-md mt-1 font-bold md:text-2xl'>
           At Fireswitch Technologies
         </p>
-        <div className='grid grid-cols-5 place-items-center gap-3 space-y-4 py-2 md:px-10'>
+        <div className='grid grid-cols-5 place-items-center gap-3 space-y-4  py-2 md:px-10'>
           <img
-            className='h-7 w-7 rounded-full'
+            className='h-7 w-7 rounded-full md:h-20 md:w-20'
             src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png'
             alt='react_icon'
           />
           <img
-            className='h-7 w-7 rounded-full'
+            className='h-7 w-7 rounded-full md:h-20 md:w-20'
             src='https://cdn.cdnlogo.com/logos/m/56/materialize-css.svg'
             alt='materilaize_logo'
           />
           <img
-            className='h-7 w-7 rounded-full'
+            className='h-7 w-7 rounded-full md:h-20 md:w-20'
             src='https://cdn.cdnlogo.com/logos/t/58/tailwindcss.svg'
             alt='tailwindcss_logo'
           />
           <img
-            className='h-7 w-7 rounded-full'
+            className='h-7 w-7 rounded-full md:h-20 md:w-20'
             src='https://cdn.cdnlogo.com/logos/c/18/css.svg'
             alt='css_logo'
           />
           <img
-            className='h-7 w-7 rounded-full'
+            className='h-7 w-7 rounded-full md:h-20 md:w-20'
             src='https://cdn.cdnlogo.com/logos/h/90/html-5.svg'
             alt='html_logo'
           />
           <img
-            className='h-7 w-7 rounded-full'
+            className='h-7 w-7 rounded-full md:h-20 md:w-20'
             src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxms_89DVmV1TlKo0oH9r3DxXe4o7fpez3Uk84kZ1GyiucnpoqR9IJUwbPWBGifosp7PY&usqp=CAU'
             alt='mongoDb'
           />
           <img
-            className='h-7 w-7 rounded-full'
+            className='h-7 w-7 rounded-full md:h-20 md:w-20'
             src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmO962KmJdNhGGZubJ92-ZNzW3sXUYMqYdffbCc0j0cn4A9R-ANdM0B15cz8nonwVnxBw&usqp=CAU'
             alt=''
           />
           <img
-            className='h-7 w-7 rounded-full'
+            className='h-7 w-7 rounded-full md:h-20 md:w-20'
             src='https://cdn.cdnlogo.com/logos/j/69/javascript.svg'
             alt='javascript_logo'
           />
 
           <img
-            className='h-7 w-7 rounded-full'
+            className='h-7 w-7 rounded-full md:h-20 md:w-20'
             src='https://e7.pngegg.com/pngimages/205/650/png-clipart-node-js-javascript-software-developer-express-js-computer-software-node-js-logo-nodejs-thumbnail.png'
             alt='nodejs'
           />
           <img
-            className='h-7 w-7 rounded-full'
+            className='h-7 w-7 rounded-full md:h-20 md:w-20'
             src='https://seeklogo.com/images/M/material-ui-logo-5BDCB9BA8F-seeklogo.com.png'
             alt=''
           />
